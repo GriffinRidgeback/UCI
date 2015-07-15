8c41c126096a2244b7007dd2b083388a03db40c6
Kevin E. D'Elia <KevinDElia@verizon.net>
2015-07-15 19:51:34 -0400

    Initial cut at creating the merged train/test dataset

---
+++ run_analysis.R
@@ -0,0 +1,17 @@
+activity_labels <- read.table("./data/activity_labels.txt", colClasses = "character")
+features <-  read.table("./data/features.txt", colClasses = "character")
+subject_train <- read.table("./data/train/subject_train.txt")
+X_train <- read.table("./data/train/X_train.txt")
+y_train <- read.table("./data/train/y_train.txt")
+subject_test <- read.table("./data/test/subject_test.txt")
+X_test <- read.table("./data/test/X_test.txt")
+y_test <- read.table("./data/test/y_test.txt")
+
+train_data  <- cbind(subject_train, y_train)
+train_data  <- cbind(train_data, X_train)
+
+test_data  <- cbind(subject_test, y_test)
+test_data  <- cbind(test_data, X_test)
+
+all_data  <- rbind(train_data, test_data)
+
