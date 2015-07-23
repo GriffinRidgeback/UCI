ff11750beefe919d826f822bd78b7a20f2a6fba5
Kevin E. D'Elia <KevinDElia@verizon.net>
2015-07-23 12:44:39 -0400

    hopefully the final versions of the run_analysis.R and ReadMe.md files

--- README.md
+++ README.md
@@ -38,56 +38,32 @@ The dataset under analysis (*UCI HAR Dataset*) is obtained from the UCI Machine 
 
 **NOTE**:  The code to do the actual download and extraction has been commented out due to the prohibitive time of the download.  Prior to executing the script, it is expected that the user of the script has downloaded the file directly from the page link (__*much*__ faster!) and extracted the contents to the following directories: *./data*, *./data/train*, and *./data/test*.  Step 1 of the script will verify the existence of this directory and exit the script should it not exist.
 
-### Step 1 - Merge the training and the test sets to create one data set
+### Step 1 - Merge the training and the test sets to create one data set  
+The script first checks for the existence of the directory *./data*, which is relative to the location of the script.  If this directory does not exist, the script exits with an appropriate message.
+Once the existence of the *./data* has been confirmed, the script will read in a total of 8 files.  A description of each file follows:
+
++ __activity_labels.txt__:  contains textual descriptions of the activities performed by the volunteer subjects (WALKING, etc.)  
++ __features.txt__: contains the names of the measurements taken during training/test (tBodyAcc-mean()-X).  Information regarding the approach to naming the measurements can be found at the UCI Machine Learning Repository^2^.  
++ __train/subject_train.txt__: contains the subject IDs which participated in the training sessions  
++ __train/y_train.txt__: contains the activity IDs which were performed by the training subjects  
++ __train/X_train.txt__: contains the measurements captured during the training sessions  
++ __train/subject_train.txt__: contains the subject IDs which participated in the testing sessions  
++ __train/y_train.txt__: contains the activity IDs which were performed by the testing subjects  
++ __train/X_train.txt__: contains the measurements captured during the testing sessions  
+
+**NOTE**: The dataset also includes Inertial Signals data for both the training and testing sessions.  These data files were not used in the analysis.
 
 ### Step 2 - Extract only the columns containing values for the mean and standard deviation of each measurement
+After the data has been read in successfully, the column headers are set using the text __SubjectID__, __Activity__, and the contents of the second column from __features.txt__.  Duplicate columns are then identified and the dataset is subsetted using the logical vector of unique columns.  Then the data is sorted first by __SubjectID__, then __Activity__.  The column names are extracted and a grep is run against them to extract the column indices for mean and standard deviation measurements. The guideline for coding the regular expression to determine which columns to keep is that any column name which includes the text __mean()__ or __std()__ was selected; all others were excluded.  This vector of indices is used to subset the original dataset to create a reduced dataset containing measurements for mean and standard deviations values only.
 
 ### Step 3 - Use descriptive activity names to name the activities in the data set
+After the original dataset has been reduced to one containing only mean and standard deviation measurements, the numerical values which are contained in the __Activity__ column are converted to textual values using the information contained in the __activity_labels.txt__ data.  The conversion is achieved through the use of a loop which iterates over the length of the __activity_labels__ vector and subsets the dataset based on loop index, replacing the __Activity__ column value of all matching rows with the corresponding entry from the __activity_labels__ vector.  
 
 ### Step 4 - Appropriately label the data set columns with descriptive variable names
+The first step in this phase of processing extracts the column headers in order to modify them to more meaningful names.  The modification steps are to replace all __-mean()-__ and __-std()-__ text with __Mean__ and __Std__, respectively.  The final modification is to prefix all measurement column headers with the Greek letter mu, which represents an arithmetic mean value, and enclose the column headers in parentheses.  The existing columns of the dataset are then overlaid with the new column headers.
 
 ### Step 5 - From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject
-
-Script checks only for the existence of the directory "./data", which is relative to the location of the script.  If this directory does not exist, the script exits with an appropriate message.
-
-Indicate that the Inertial files were not used; the files you used are a sample of that data
-
-
-
-Add to the submission box:
-"tidy data as per the ReadMe that can be read into R with read.table(header=TRUE)"
-
-
-
-Make sure:
-1. headings clearly identify which columns are which
-2. variables are in different columns
-3. no duplicates (check!)
-
-
-
-
-
-
-Tidy data is not made to be looked neatly at in programs like notepad (which is often the default for text files on windows), but if you saved the file with write.table according to the instructions, the command for reading it in and looking at it in R would be
-
-    data <- read.table(file_path, header = TRUE) #if they used some other way of saving the file than a default write.table, this step will be different
-    View(data)
-    
-   
-   Over in the FAQ thread I mentioned people could put the code for reading the tidy data into R into their readMe
-
-But if you want to get fancier than that:
-
-If you right click on a file name in the submission box and copy the web address, you can paste it into a script.
-
-   address <- "https://s3.amazonaws.com/coursera-uploads/user-longmysteriouscode/asst-3/massivelongcode.csv"
-address <- sub("^https", "http", address)
-data <- read.table(url(address), header = TRUE) #if they used some other way of saving the file than a default write.table, this step will be different
-View(data)
-
-
-
+The final step in the processing is to do the analysis.  The dataset is grouped by __SubjectID__ and __Activity__ and the columns associated with the grouping are summarized via the mean function.  The output of the analysis is written to the local file system using __write.table__ with no specific options other than the defaults; the name of the output file is __tidy_data.txt__ and can be read back into R for verification using __read.table("./tidy_data.txt", header=TRUE)__.
 
 
 ##### __References__:
--- run_analysis.R
+++ run_analysis.R
@@ -8,70 +8,131 @@ library(dplyr)
 #   unzip("data.zip", exdir = "./data")  # will extract data.zip into folder UCI HAR Dataset
 # }
 
-# 1 - Merges the training and the test sets to create one data set.
-
 if (!file.exists("./data")) {
     stop("The directory data does not exist in the current directory.")
 }
 
-readline(prompt="Press [enter] to continue... ")
+#------------------------------------------------------------------
+# 1 - Merges the training and the test sets to create one data set.
+#------------------------------------------------------------------
+readline(prompt="Step 1 will read in data and perform the merge; press [enter] to continue... ")
 
+# activity lables (WALKING, etc.)
 activity_labels <- read.table("./data/activity_labels.txt", colClasses = "character")
-features <-  read.table("./data/features.txt", colClasses = "character")
 
+# feature labels (tBodyAcc-mean()-X, etc.)
+features <-  read.table("./data/features.txt", stringsAsFactors = F, colClasses = "character")
+
+# Subject IDs for the training data
 subject_train <- read.table("./data/train/subject_train.txt", colClasses = "numeric")
+
+# Measurement data from the training cycle
 X_train <- read.table("./data/train/X_train.txt", colClasses = "numeric")
+
+# Activity IDs for the training data
 y_train <- read.table("./data/train/y_train.txt", colClasses = "numeric")
 
+# Subject IDs for the test data
 subject_test <- read.table("./data/test/subject_test.txt", colClasses = "numeric")
+
+# Measurement data from the test cycle
 X_test <- read.table("./data/test/X_test.txt", colClasses = "numeric")
+
+# Activity IDs for the test data
 y_test <- read.table("./data/test/y_test.txt", colClasses = "numeric")
  
+readline(prompt="All data files read. Now perform the merge; press [enter] to continue... ")
+
+# perform a column-wise bind of the training subject and activity IDs
 train_data  <- cbind(subject_train, y_train)
+
+# perform a column-wise bind of the two columns and the training measurements
 train_data  <- cbind(train_data, X_train)
 
+# perform a column-wise bind of the test subject and activity IDs
 test_data  <- cbind(subject_test, y_test)
+
+# perform a column-wise bind of the two columns and the testing measurements
 test_data  <- cbind(test_data, X_test)
 
+# perform the final merge of the training and test data
 all_data  <- rbind(train_data, test_data)
 
-measurement_names <- read.table("./data/features.txt", stringsAsFactors = F)
+readline(prompt="All data files merged into one dataset. Press [enter] to continue... ")
 
-names(all_data)  <- c("SubjectID", "Activity", measurement_names$V2)
+#--------------------------------------------------------------------------------------------
+# 2 - Extracts only the measurements on the mean and standard deviation for each measurement. 
+#--------------------------------------------------------------------------------------------
+readline(prompt="Step 2 will extract only mean/standard deviation measurements; press [enter] to continue... ")
+
+# set the column headers
+names(all_data)  <- c("SubjectID", "Activity", features$V2)
+
+# remove duplicate column names
+all_data <- all_data[, !duplicated(colnames(all_data))]
 
 # put the data in order
 all_data_ordered  <- all_data[with(all_data, order(SubjectID, Activity)), ]
 
-# 2 - Extracts only the measurements on the mean and standard deviation for each measurement. 
-# get the names for excluding duplicates
+# extract the column names
 names <- colnames(all_data_ordered)
 
-# now show that none of the columns of interest are duplicated
+# grep for the columns of interest; selected_columns will contain the 
+# indices of the columns which match the regular expressions
 selected_columns  <- grep(".*mean\\(.*|.*std\\(.*", names)
 
-# and the test
-length(names[selected_columns]) == length(unique(names)[selected_columns])
-
-# Now it has been shown that none of the columns
-# of interest are duplicates, so the selected_columns vector
-# can be used to subset the full dataset in order to create
-# a dataset with only columns containing mean and std
-# measurements, which is ready to be relabeled and further reduced.
+# the selected_columns vector can be used to subset the full dataset 
+# in order to create a dataset with only columns containing mean and 
+# std measurements, which is ready to be relabeled and further reduced.
 all_data_ordered  <- all_data_ordered[, c(1:2, selected_columns)]
 
-# 3 - Uses descriptive activity names to name the activities in the data set
+readline(prompt="Mean/Standard deviation measurements extracted successfully. Press [enter] to continue... ")
+
+#----------------------------------------------------------------------------
+# 3 - Uses descriptive activity names to name the activities in the data set.
+#----------------------------------------------------------------------------
+readline(prompt="Step 3 will appropriately name the activities in the data set; press [enter] to continue... ")
+
+# replace the numerical activity value for all matching with a textual representation
 for (i in 1:length(activity_labels$V2)) {
     all_data_ordered[all_data_ordered$Activity == i, 2]  <- activity_labels$V2[i]
 }
 
+readline(prompt="Descriptive Activity labels applied to dataset. Press [enter] to continue... ")
+
+#-----------------------------------------------------------------------
 # 4 - Appropriately labels the data set with descriptive variable names. 
+#-----------------------------------------------------------------------
+readline(prompt="Step 4 will appropriately name the columns in the data set; press [enter] to continue... ")
+
+# create a new vector to hold the modified column headers
 new_names  <- colnames((all_data_ordered))
+
+# modify headers for mean values
 new_names  <- gsub("-mean\\(\\)-", "Mean", new_names)
 new_names  <- gsub("-mean\\(\\)", "Mean", new_names)
+
+# modify headers for standard deviation values
 new_names  <- gsub("-std\\(\\)-", "Std", new_names)
 new_names  <- gsub("-std\\(\\)", "Std", new_names)
-names(all_data_ordered)  <- new_names
 
-# 5 - From the data set in step 4, creates a second, independent tidy data 
-# set with the average of each variable for each activity and each subject.
+# add indicator to show that all values are means
+mean_names  <- paste("μ(", new_names[3:length(new_names)], ")", sep = "")
+
+# set the new column headers
+names(all_data_ordered)  <- c(new_names[1:2], mean_names)
+
+readline(prompt="Descriptive Variable labels (column headers) applied to dataset. Press [enter] to continue... ")
+
+#--------------------------------------------------------------------------
+# 5 - From the data set in step 4, creates a second, independent tidy data.
+#--------------------------------------------------------------------------
+readline(prompt="Step 5 will create the tidy data; press [enter] to continue... ")
+
+# create tidy dataset with the average of each variable for each activity and each subject.
 tidy_data <- group_by(all_data_ordered, SubjectID, Activity) %>% summarise_each(funs(mean))
+
+# write out the tidy data
+write.table(tidy_data, "./tidy_data.txt")
+
+readline(prompt="Analysis of data completed; tidy data set produced and written to local file system. Press [enter] to exit the script... ")
